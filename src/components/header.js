a0d7dc4d4ebeecdc7e59cf3d2b8d0c3fe6fd5e08
Dirrbick <kory.a.jaxn@gmail.com>
2021-03-04 12:04:10 -0800

    initial portfolio build

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import { Link } from "@material-ui/core";
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1>Kory Jackson</h1>
+      <h3>Chef turned Software Developer</h3>
+    </>
+  )
+}
+
+export default Header;
